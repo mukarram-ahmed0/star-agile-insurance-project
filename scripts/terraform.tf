7700e0b5f9418f0460f3ea3e9d35a0ed4deaeaaa
mukarram-ahmed0 <161357378+mukarram-ahmed0@users.noreply.github.com>
2024-05-17 03:21:42 +0530

    Create terraform.tf

---
+++ scripts/terraform.tf
@@ -0,0 +1,24 @@
+resource "aws_instance" "test-server" {
+  ami           = "ami-07caf09b362be10b8" 
+  instance_type = "t2.micro" 
+  key_name = "jenkinm"
+  vpc_security_group_ids= ["sg-0c6ce131ed5f2c0c5"]
+  connection {
+    type     = "ssh"
+    user     = "ec2-user"
+    private_key = file("./jenkinm.pem")
+    host     = self.public_ip
+  }
+  provisioner "remote-exec" {
+    inline = [ "echo 'wait to start instance' "]
+  }
+  tags = {
+    Name = "test-server"
+  }
+  provisioner "local-exec" {
+        command = " echo ${aws_instance.test-server.public_ip} > inventory "
+  }
+   provisioner "local-exec" {
+  command = "ansible-playbook /var/lib/jenkins/workspace/banking/scripts/finance-playbook.yml "
+  } 
+}
